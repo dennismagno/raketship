6802f8cbc1bd6bec13edf07c5eab90b1fa13da71
pandude <dimagnoph@gmail.com>
2019-04-08 23:30:57 +0800

    Initial commit

---
+++ Raketship/App_Start/BundleConfig.cs
@@ -0,0 +1,55 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Raketship
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/corejquery").Include(
+                        "~/Scripts/core/jquery.3.2.1.js",
+                        "~/Scripts/core/popper.js",
+                        "~/Scripts/core/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/plugin/jquery-ui-1.12.1.custom/jquery-ui.js",
+                        "~/Scripts/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryscollbar").Include(
+                        "~/Scripts/plugin/jquery-scrollbar/jquery.scrollbar.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/atlantisjs").Include(
+                   "~/Scripts/atlantis.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/bootstrap.css",
+                        "~/Content/atlantis.css",
+                        "~/Content/demo.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+        }
+    }
+}
---
+++ Raketship/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Raketship.MvcApplication" Language="C#" %>
