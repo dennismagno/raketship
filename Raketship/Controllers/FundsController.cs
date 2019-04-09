7b4eacffc93668a85331d624c27451cbe43ed847
pandude <dimagnoph@gmail.com>
2019-04-10 01:42:20 +0800

    Added fund screen and invoice

---
+++ Raketship/Controllers/FundsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Raketship.Controllers
+{
+    public class FundsController : Controller
+    {
+        //
+        // GET: /Funds/
+
+        public ActionResult Index()
+        {
+            ViewBag.ActiveNavItem = "funds";
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            ViewBag.ActiveNavItem = "funds";
+            return View();
+        }
+
+        public ActionResult Withdraw()
+        {
+            ViewBag.ActiveNavItem = "funds";
+            return View();
+        }
+    }
+}
---
+++ Raketship/Controllers/InvoiceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Raketship.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        //
+        // GET: /Invoice/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
